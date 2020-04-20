a5887d8edc0cc71e7b105b8fca60ee39837e3d9f
Thomas Dye <thomasallendye@gmail.com>
2020-04-20 17:37:38 -0400

    Initial commit

---
+++ Animal Crossing New Horizons Guide/Animal Crossing New Horizons Guide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animal Crossing New Horizons Guide
+//
+//  Created by Thomas Dye on 4/20/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
