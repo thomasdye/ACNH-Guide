5e5b2b59f1d1098d39e2187783df40cb216991af
Thomas Dye <thomasallendye@gmail.com>
2020-04-22 08:36:23 -0400

    Starting on the Bugs

---
+++ Animal Crossing New Horizons Guide/Animal Crossing New Horizons Guide/Bug.swift
@@ -0,0 +1,20 @@
+//
+//  Bug.swift
+//  Animal Crossing New Horizons Guide
+//
+//  Created by Thomas Dye on 4/22/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+import UIKit
+
+struct Bug {
+    var name: String?
+    var image: UIImage?
+    var price: Int?
+    var location: String?
+    var time: String?
+    var months: String?
+    var hasBeenCaught: Bool?
+}
+
